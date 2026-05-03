c4c7a9da58e2834388ceec1e4a2dfe4ed3361011
Prasanna Aravindan <paravindan@cavallotechnologies.ca>
2026-05-03 03:23:29 -0700

    add: utility functions & prompts

---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+"""
+This file marks the src/ directory as a Python package - an explicit signal to Python that this is a package and to allow for absolute imports. It can be left empty or used for package-level
+"""
